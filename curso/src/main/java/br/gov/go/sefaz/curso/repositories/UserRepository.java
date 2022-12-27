f3dfb486c31ae8235ad34ea38468818a24653f77
Matheus Duarte Albuquerque <90364847+Mathdal002@users.noreply.github.com>
2022-12-27 17:56:18 -0300

    Add files via upload
    Projeto Spring-Boot

---
+++ curso/src/main/java/br/gov/go/sefaz/curso/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.gov.go.sefaz.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.gov.go.sefaz.curso.entidades.User;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+
+}
